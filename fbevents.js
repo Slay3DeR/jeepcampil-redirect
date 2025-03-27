a63082e4bbe1fa5aed3070747c168df350069a20
Slay3DeR <yaffeofer@gmail.com>
2025-03-27 17:26:47 +0200

    Delete fbevents.js

--- fbevents.js
+++
@@ -1,8 +0,0 @@
-fetch('https://connect.facebook.net/en_US/fbevents.js', { mode: 'no-cors' })
-.then(response => response.text())
-.then(scriptText => {
-    let script = document.createElement('script');
-    script.innerHTML = scriptText;
-    document.head.appendChild(script);
-})
-.catch(error => console.log("⚠️ Facebook Pixel נחסם על ידי AdBlock!", error));
